8305b1540e0db81b5c03f47969ba51bfaf16b32e
Marshnme <jildsmiles@yahoo.com>
2022-06-17 14:14:34 -0400

    rough auto scrolling working

--- ImageSlider/script.js
+++ ImageSlider/script.js
@@ -1,8 +1,25 @@
 let navButtons = document.querySelectorAll("a");
-
+let imga = document.querySelectorAll(".slider-img");
+let imgs = [...imga]
 navButtons.forEach(item => {
     item.addEventListener('click',activeTab)
 })
+
+let i = 0
+function autoScroll(imgC){
+    console.log(imgs.indexOf(imgC))
+    i = imgs.indexOf(imgC) + 1
+    if(imgs.indexOf(imgC) == 5){
+            i = 0
+        }
+        
+        navButtons[i].click()
+
+        
+
+    console.log("da index",i)
+    }
+
 
 
 function activeTab(e){
@@ -13,6 +30,7 @@ function activeTab(e){
             item.classList.remove("background")
         })
         e.target.classList.add("background")
+        
 }
 
 
@@ -36,84 +54,102 @@ let imagesContainer = document.querySelector(".images-container")
 imagesContainer.addEventListener("scroll",debounce(moveNavButtons))
 
 function moveNavButtons(e){
-    let imgs = document.querySelectorAll(".slider-img");
     
     function rectEle(ele){
         return ele.getBoundingClientRect()
     }
         
     let currtargetrect = e.target.getBoundingClientRect()
-    // timeout interval make certain image lineup with 
-    // currtargetrect.x
-    console.log(currtargetrect.x,imgs[1].x)
-    navButtons.forEach(item =>{
-        
+    
+    
         switch(true){
             case currtargetrect.left < rectEle(imgs[0]).left && currtargetrect.right > rectEle(imgs[0]).right:
-                if(item.hash == "#one"){
-                    item.classList.add("background")
-                    // setInterval(function(){
-                    //     e.target.scrollLeft = rectEle(imgs[1]).left
-                    // },6000)
+                
+                navButtons.forEach(item =>{
+                    if(item.hash == "#one"){
+                    item.classList.add("background");
+                    
+                    setTimeout(autoScroll,8000,imgs[0])
+                        
                 }else{
                     item.classList.remove("background")
                 }
+                
+                })
+                 
+                
             break
             case currtargetrect.left < rectEle(imgs[1]).left && currtargetrect.right > rectEle(imgs[1]).right:
-               
-                if(item.hash == "#two"){
-                    // setInterval(function(){
-                    //     e.target.scrollLeft = rectEle(imgs[2]).left
-                    // },6000)
-                    item.classList.add("background")
+                
+                navButtons.forEach(item =>{
+                    if(item.hash == "#two"){
+                    item.classList.add("background");
+                    
+                    setTimeout(autoScroll,8000,imgs[1])
+                        
                 }else{
                     item.classList.remove("background")
                 }
+                })
+                
+                
             break
             case currtargetrect.left < rectEle(imgs[2]).left && currtargetrect.right > rectEle(imgs[2]).right:
-                // setInterval(function(){
-                //     e.target.scrollLeft = rectEle(imgs[3]).left
-                // },6000)
-                if(item.hash == "#three"){
+                
+                navButtons.forEach(item =>{
+                    if(item.hash == "#three"){
+                    item.classList.add("background");
                     
-                    item.classList.add("background")
+                    setTimeout(autoScroll,8000,imgs[2])
+                        
                 }else{
                     item.classList.remove("background")
                 }
+                })
+                
+                
             break
             case currtargetrect.left < rectEle(imgs[3]).left && currtargetrect.right > rectEle(imgs[3]).right:
-                // setInterval(function(){
-                //     e.target.scrollLeft = rectEle(imgs[4]).left
-                // },6000)
-                if(item.hash == "#four"){
+                
+                navButtons.forEach(item =>{
+                    if(item.hash == "#four"){
+                    item.classList.add("background");
+                    setTimeout(autoScroll,8000,imgs[3])
                     
-                    item.classList.add("background")
                 }else{
                     item.classList.remove("background")
                 }
+                })
+                
+                
             break
             case currtargetrect.left < rectEle(imgs[4]).left && currtargetrect.right > rectEle(imgs[4]).right:
-                // setInterval(function(){
-                //     e.target.scrollLeft = rectEle(imgs[5]).left
-                // },6000)
-                if(item.hash == "#five"){
-                    
-                    item.classList.add("background")
+                
+                navButtons.forEach(item =>{
+                     if(item.hash == "#five"){
+                    item.classList.add("background");
+                    setTimeout(autoScroll,8000,imgs[4])
                 }else{
                     item.classList.remove("background")
                 }
+                })
+                
+               
             break
             case currtargetrect.left < rectEle(imgs[5]).left && currtargetrect.right > rectEle(imgs[5]).right:
-                // setInterval(function(){
-                //     e.target.scrollLeft = rectEle(imgs[0]).left
-                // },6000)
-                if(item.hash == "#six"){
-                    
-                    item.classList.add("background")
+                
+                
+                navButtons.forEach(item =>{
+                    if(item.hash == "#six"){
+                    item.classList.add("background");
+                    setTimeout(autoScroll,8000,imgs[5])
                 }else{
                     item.classList.remove("background")
                 }
+                })
+                
+              
             break  
         }
-    })
+    
 }
